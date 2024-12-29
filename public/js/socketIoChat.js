4bdc9f0494198707bd0ae410d16c3207b48fc392
RoyyertDev <royyertalfonso@gmail.com>
2024-12-28 20:27:06 -0400

    add fix aside

---
+++ public/js/socketIoChat.js
@@ -0,0 +1,52 @@
+import { io } from "https://cdn.socket.io/4.5.1/socket.io.esm.min.js";
+
+const socket = io();
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const messages = document.getElementById("messages");
+
+socket.on("message", (message) => {
+  let active = messages.querySelector(".active");
+
+  if (!active) {
+    const item = `
+                  <li class="active flex ${
+                    message.from === "User" ? "flex-row-reverse" : "flex-row"
+                  } w-full gap-4 items-end text-slate-600">
+                      <img src="https://www.clarin.com/img/2023/12/01/rhVeUAooY_2000x1500__1.jpg" alt=""
+                          class="w-12 h-12 object-cover rounded-full">
+                      <div class="grid grid-rows-[auto_1fr] rounded-3xl ${
+                        message.from === "User"
+                          ? "rounded-br-none bg-slate-200 shadow-sm py-3 px-4"
+                          : "rounded-bl-none"
+                      } text-wrap gap-1">
+                          <p class="message-content"></p>
+                          <span class="text-xs font-semibold">${
+                            message.from === "User" ? "Tu" : "RoSa"
+                          } · ${new Date().toLocaleTimeString()}</span>
+                      </div
+                  </li>
+              `;
+    messages.insertAdjacentHTML("beforeend", item);
+    active = messages.querySelector(".active");
+  }
+  const p = active.querySelector(".message-content");
+  p.textContent += message.message;
+  messages.scrollTop = messages.scrollHeight;
+  if (message.complete) {
+    const activeLi = messages.querySelector(".active");
+    if (activeLi) {
+      activeLi.classList.remove("active");
+    }
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input.value !== "") {
+    socket.emit("message", input.value);
+    input.value = "";
+  }
+});
--- server.js
+++ server.js
@@ -23,6 +23,7 @@ io.on("connection", (socket) => {
 
 app.use(logger("dev"));
 app.use(express.static(path.join(process.cwd(), "public")));
+app.use("/components", express.static(path.join(process.cwd(), "components")));
 app.use("/", router);
 server.listen(3000, () => {
   console.log(`Corriendo en el puerto http://localhost:${PORT}`);
